7512eeff846d4c5f07596fa511a954cfad499d78
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2024-12-24 01:52:20 +0530

    chore: add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,12 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
+				<link rel='icon' href='https://fav.farm/👨‍💻' />
 				<title>Sanjeev Sharma - Frontend Engineer</title>
 				<meta name='title' content='Sanjeev Sharma - Frontend Engineer' />
 				<meta
 					name='description'
-					content='Sanjeev Sharma is a frontend engineer with 4+ years of experience who specializes in JavaScript and React.'
+					content='Sanjeev Sharma is a frontend engineer with 5+ years of experience who specializes in JavaScript and React.'
 				/>
 			</Head>
 			<body>
